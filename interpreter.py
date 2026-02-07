ff1f699d4001785aa1d1d47c57b7e05f23a084ea
Zander Lewis <zander@zanderlewis.dev>
2026-02-07 14:30:59 +0000

    bounds checks

--- interpreter.py
+++ interpreter.py
@@ -10,43 +10,55 @@ sine = lambda l: math.sin(int(l[1]))
 cosine = lambda l: math.cos(int(l[1]))
 tangent = lambda l: math.tan(int(l[1]))
 
-def handle_line(line, func):
+# This function allows us to write less code in order
+# to handle a line and call their respective functions.
+def handle_line(line, func, num_args, lines):
+    check_bounds(line, num_args, lines)
     print(func(line))
 
-# This function makes sure that enough
-# arguments are passed to the interpreter
-# allowing us to avoid crashing the interpreter.
-# `n` is the number of expected arguments for the keyword.
-def check_bounds(line, n):
-    pass
+# This function makes sure that enough arguments
+# are passed to the interpreter allowing us to
+# avoid crashing the interpreter. `n` is the
+# number of expected arguments for the keyword.
+def check_bounds(line, n, lines):
+    try:
+        _ = line[n]
+    except IndexError:
+        print(f"error: not enough arguments on line {lines}")
+        sys.exit(1)
 
+
+# This function interprets our language code and ultimately runs it.
 def interpret(code):
     lines = 0
     for line in code.splitlines():
         lines += 1
         line = line.split()
         match line[0]:
-            case "ADD": handle_line(line, add)
-            case "SUB": handle_line(line, sub)
-            case "MUL": handle_line(line, mul)
+            case "ADD": handle_line(line, add, 2, lines)
+            case "SUB": handle_line(line, sub, 2, lines)
+            case "MUL": handle_line(line, mul, 2, lines)
             case "DIV":
                 if line[2] == "0":
                     print(f"error: div by zero on line {lines}")
                 else:
-                    handle_line(line, div)
-            case "SIN": handle_line(line, sine)
-            case "COS": handle_line(line, cosine)
-            case "TAN": handle_line(line, tangent)
+                    handle_line(line, div, 2, lines)
+            case "SIN": handle_line(line, sine, 1, lines)
+            case "COS": handle_line(line, cosine, 1, lines)
+            case "TAN": handle_line(line, tangent, 1, lines)
 
 def main():
     if len(sys.argv) < 2:
         print("Loading REPL...")
-        print("Type 'exit' to exit.")
-        while True:
-            line = input(">>> ")
-            if line == "exit":
-                break
-            interpret(line)
+        print("Type 'exit' or 'quit' to exit.")
+        try:
+            while True:
+                line = input(">>> ")
+                if line.strip() in ["exit", "quit"]:
+                    break
+                interpret(line)
+        except KeyboardInterrupt:
+            sys.exit(0)
     else:
         interpret(open(sys.argv[1], 'r').read())
 
